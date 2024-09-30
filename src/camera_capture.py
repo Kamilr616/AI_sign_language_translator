88939dbd27f4ca074bc1e73128d3e4709522e2eb
Kamil Rataj <kamilr616@gmail.com>
2024-09-30 11:37:00 +0200

    camera thread

--- src/camera_capture.py
+++ src/camera_capture.py
@@ -4,14 +4,14 @@ import cv2
 import time
 import warnings
 
-
 class AsyncCamera:
-    def __init__(self, fd=1, **kwargs):
+    def __init__(self, fd=1, queue_size=10, **kwargs):
         """
         Initialize the AsyncCamera instance.
 
         Args:
             fd (int): File descriptor for the camera. Defaults to 1.
+            queue_size (int): Maximum number of frames to store in the queue.
             **kwargs: Additional keyword arguments to set camera properties.
         """
         # Set attributes from kwargs
@@ -26,13 +26,15 @@ class AsyncCamera:
         s_int(self, "width")
         s_int(self, "height")
 
-        # Initialize queues for frame processing
-        self.q = queue.Queue()
+        # Initialize queues for frame processing with a defined size
+        self.q = queue.Queue(maxsize=queue_size)
         self.q2 = queue.Queue()
 
         # Start the camera capture thread
         self.t = threading.Thread(target=AsyncCamera.func, args=(
-        self.q, self.q2, fd, {"fps": self.fps, "width": self.width, "height": self.height, "format": self.format}))
+            self.q, self.q2, fd, {"fps": self.fps, "width": self.width, "height": self.height, "format": self.format}
+        ))
+        self.t.daemon = True
         self.t.start()
         self.current = None
 
@@ -49,10 +51,7 @@ class AsyncCamera:
         Returns:
             bool: Returns False if thread has ended.
         """
-        if not self.t.is_alive():
-            return True
-        else:
-            return False
+        return not self.t.is_alive()
 
     @staticmethod
     def func(q, q2, fd, opt):
@@ -65,13 +64,14 @@ class AsyncCamera:
             fd (int): File descriptor for the camera.
             opt (dict): Dictionary of camera options.
         """
+        v = None
         try:
             # Open the video capture with the given file descriptor
             v = cv2.VideoCapture(fd)
             # Set video capture properties
             v.set(cv2.CAP_PROP_FOURCC,
                   (ord(opt['format'][0]) << 0) + (ord(opt['format'][1]) << 8) + (ord(opt['format'][2]) << 16) + (
-                              ord(opt['format'][3]) << 24))
+                          ord(opt['format'][3]) << 24))
             v.set(cv2.CAP_PROP_FPS, opt["fps"])
             v.set(cv2.CAP_PROP_FRAME_WIDTH, opt["width"])
             v.set(cv2.CAP_PROP_FRAME_HEIGHT, opt["height"])
@@ -79,14 +79,23 @@ class AsyncCamera:
             while v.isOpened():
                 if not q2.empty():
                     return
+
                 # Read a frame from the video capture
                 stat, src = v.read()
                 if stat:
-                    if q.empty():
-                        src = cv2.cvtColor(src, cv2.COLOR_BGR2RGB)
+                    if not q.full():
+                        # Add new frame to the queue
                         q.put((time.time_ns(), src))
+                    else:
+                        # Discard the oldest frame and add the new one
+                        q.get()
+                        q.put((time.time_ns(), src))
+
         except Exception as e:
             warnings.warn(f"Exception in camera capture: {e}")
+        finally:
+            if v is not None:  # Check if v was successfully assigned
+                v.release()  # Release the video capture object when done
 
     def read(self):
         """
---
+++ src/main_app.py
@@ -0,0 +1,178 @@
+from recognizer import GestureRecognizerApp
+from gui import *
+from speaker import TextToSpeech
+from camera_capture import AsyncCamera
+
+MODEL_PATH = '../models/gesture_recognizer_asl_6.task'
+CAMERA_ID = 0
+CAMERA_WIDTH = 640
+CAMERA_HEIGHT = 480
+CAMERA_FPS = 30
+
+
+class MainApp(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        """
+        Initialize the main application window and connect UI elements to their respective methods.
+        """
+        super(MainApp, self).__init__()
+        self.setupUi(self)
+
+        self.camera_app = None
+        self.recognizer_app = None
+        self.tts_app = None
+        self.checkbox_1_flag = False
+
+        # Connect UI elements to their respective methods
+        self.checkBox_speak.stateChanged.connect(self.checkbox_speak_change)
+        self.pushButton_plus.clicked.connect(self.pushbutton_plus_click)
+        self.pushButton_resetRecognizer.clicked.connect(self.reset_recognizer)
+        self.pushButton_resetTTS.clicked.connect(self.reset_tts)
+        self.pushButton_speak.clicked.connect(self.pushbutton_speak_click)
+        self.pushButton_resetCap.clicked.connect(self.pushbutton_reset_cap_click)
+
+    def reset_camera(self):
+        """
+        Reset the camera application with new settings.
+        """
+        if self.camera_app is not None:
+            self.camera_app.destroy()
+            self.camera_app = None
+        try:
+            self.camera_app = AsyncCamera(fd=CAMERA_ID, queue_size=5, fps=CAMERA_FPS, width=CAMERA_WIDTH,
+                                          height=CAMERA_HEIGHT, format='mjpeg')
+        except Exception as e:
+            print(f"Error resetting camera: {e}")
+
+    def reset_tts(self):
+        """
+        Reset the text-to-speech engine with new settings.
+        """
+        if self.tts_app is not None:
+            self.tts_app.stop()
+
+        self.tts_app = TextToSpeech(
+            rate=self.spinBox_ttsRate.value(),
+            volume=(self.spinBox_volume.value() / 100.0))
+
+    def reset_recognizer(self):
+        """
+        Reset the gesture recognizer with new settings.
+        """
+        if self.recognizer_app is not None:
+            if self.recognizer_app.result_ready_signal:
+                self.recognizer_app.result_ready_signal.disconnect()
+            self.recognizer_app.close()
+            self.recognizer_app = None
+
+        if self.camera_app is not None:
+            self.recognizer_app = GestureRecognizerApp(
+                model=MODEL_PATH,
+                num_hands=1,
+                min_hand_detection_confidence=(self.spinBox_detection.value() / 100.0),
+                min_hand_presence_confidence=(self.spinBox_presence.value() / 100.0),
+                min_tracking_confidence=(self.spinBox_tracking.value() / 100.0),
+                score_confidence=(self.spinBox_treshold.value() / 100.0),
+                camera=self.camera_app
+            )
+            self.recognizer_app.result_ready_signal.connect(self.update_frame)
+            self.recognizer_app.start()
+            self.recognizer_app.recognize_frame()
+
+    def start(self):
+        """
+        Start the gesture recognizer application if not already started.
+        """
+        if not self.camera_app:
+            self.reset_camera()
+
+        if not self.tts_app:
+            self.reset_tts()
+
+        if not self.recognizer_app:
+            self.reset_recognizer()
+
+    def update_frame(self, frame, text, scores, latest_fps):
+        """
+        Update the UI with the processed frame and recognized gesture text.
+
+        Args:
+            frame (QPixmap): The processed frame.
+            text (list): Recognized gesture text.
+            scores (list): Scores of the recognized gestures.
+            latest_fps (float): The latest frames per second (FPS) value.
+        """
+        if frame:
+            self.label_displayFrame.setPixmap(frame)
+
+        if latest_fps:
+            self.label_displayFPS.setText(f'{latest_fps:.1f} FPS')
+
+        if text and scores:
+            self.label_recognitionInfo.setText(f'Sign: {text[0]} ({scores[0]:.0%}) - Hand: {text[1]} ({scores[1]:.0%})')
+            self.label_displaySign.setText(text[0])
+            self.progressBar_1.setValue(scores[0] * 100)
+
+            if self.checkbox_1_flag:
+                self.translate_to_speech(text[0])
+        else:
+            self.label_recognitionInfo.setText('No info')
+            self.label_displaySign.setText('-')
+            self.progressBar_1.setValue(0)
+
+    def translate_to_speech(self, data):
+        """
+        Translate the recognized gesture text to speech.
+
+        Args:
+            data (str): The recognized gesture text.
+        """
+        if self.tts_app is not None:
+            self.tts_app.speak(data)
+
+    def checkbox_speak_change(self):
+        """
+        Toggle the auto mode for text-to-speech.
+        """
+        self.checkbox_1_flag = not self.checkbox_1_flag
+
+    def pushbutton_speak_click(self):
+        """
+        Manually trigger text-to-speech translation.
+        """
+        self.translate_to_speech(self.label_displaySign.text())
+
+    def pushbutton_plus_click(self):
+        """
+        Placeholder for future functionality.
+        """
+        pass
+
+    def pushbutton_reset_cap_click(self):
+        """
+        Reset Camera.
+        """
+        pass
+
+    def closeEvent(self, event):
+        """
+        Release resources when closing the application.
+
+        Args:
+            event (QCloseEvent): The close event.
+        """
+        if self.recognizer_app is not None:
+            if self.recognizer_app.result_ready_signal:
+                self.recognizer_app.result_ready_signal.disconnect()
+            self.recognizer_app.close()
+            self.recognizer_app = None
+
+        if self.camera_app is not None:
+            self.camera_app.destroy()
+            self.camera_app = None
+
+        if self.tts_app is not None:
+            self.tts_app.stop()
+            self.tts_app = None
+
+        super().closeEvent(event)
