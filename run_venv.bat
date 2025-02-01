bdab1ede173e6b70e88197036c9f469fb9bd0340
Kamil Rataj <105726592+Kamilr616@users.noreply.github.com>
2025-02-01 19:06:17 +0100

    directories ogranized

--- run_venv.bat
+++ run_venv.bat
@@ -1,6 +1,10 @@
 @echo off
+
+REM Change directory
+cd .\src
+
 REM Activate the virtual environment
-call .\venv\Scripts\activate.bat
+call ..\venv\Scripts\activate.bat
 
 REM Run the Python script
-python .\src\main.py
+python .\main.py
